225b5f9951e49c4f8bf80e6d5d2de3a41b64b5f7
Nik Varga <ndv2003.ndv@gmail.com>
2025-09-06 13:12:35 -0400

    add gitignore, requirements.txt

---
+++ giano_teensy/src/audio_main.cpp
@@ -0,0 +1,25 @@
+// #include <Arduino.h>
+// #include <Audio.h>
+// #include <Wire.h>
+// #include <SPI.h>
+// #include <SD.h>
+// #include <SerialFlash.h>
+// #include <Bounce.h>
+// // put function declarations here:
+
+// AudioSynthWaveform waveform1;
+// AudioSynthWaveformSineModulated sine1;
+// AudioOutputI2S i2s1;
+// AudioConnection myConnection(sine1, i2s1);
+// void setup() {
+//   // put your setup code here, to run once:
+  
+//   Serial.begin(115200);
+
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+//   sine1.frequency(440);
+// }
+
