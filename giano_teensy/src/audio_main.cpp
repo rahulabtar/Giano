18762ab10531c2d67663cbd4949c340199def209
Nik Varga <ndv2003.ndv@gmail.com>
2025-09-08 20:24:42 -0400

    edit platformio ini, audio main

--- giano_teensy/src/audio_main.cpp
+++ giano_teensy/src/audio_main.cpp
@@ -1,25 +1,30 @@
-// #include <Arduino.h>
-// #include <Audio.h>
-// #include <Wire.h>
-// #include <SPI.h>
-// #include <SD.h>
-// #include <SerialFlash.h>
-// #include <Bounce.h>
-// // put function declarations here:
+#ifdef TEENSYDUINO
 
-// AudioSynthWaveform waveform1;
-// AudioSynthWaveformSineModulated sine1;
-// AudioOutputI2S i2s1;
-// AudioConnection myConnection(sine1, i2s1);
-// void setup() {
-//   // put your setup code here, to run once:
+#include <Arduino.h>
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+#include <Bounce.h>
+// put function declarations here:
+
+AudioSynthWaveform waveform1;
+AudioSynthWaveformSineModulated sine1;
+AudioOutputI2S i2s_1;
+AudioConnection myConnection(sine1, i2s_1);
+void setup() {
   
-//   Serial.begin(115200);
+  Serial.begin(115200);
+  sine1.frequency(440);
+  i2s_1.begin();
+  F
 
-// }
+}
 
-// void loop() {
-//   // put your main code here, to run repeatedly:
-//   sine1.frequency(440);
-// }
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
 
+#endif
