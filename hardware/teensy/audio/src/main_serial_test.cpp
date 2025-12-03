45a96d27f7d174c3cdabcfe3c57235b8829425c9
Nik Varga <ndv2003.ndv@gmail.com>
2025-12-02 23:20:09 -0500

    baller

---
+++ hardware/teensy/audio/src/main_serial_test.cpp
@@ -0,0 +1,15 @@
+#include "midi_process.h"
+#include <Arduino.h>
+
+// TODO: remember you have stashed changes
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+}
+
+void loop() {
+  if(usbMIDI.read()) {
+    Serial.print("NOTE RECEIVED: PROCESSING NOW");
+    processMIDIData();
+  }
+}
