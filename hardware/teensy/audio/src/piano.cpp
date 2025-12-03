141e5db61f5691b828d351768ef1bd441d3cbef1
nouriyaalsumait <alsumait.no@northeastern.edu>
2025-12-02 23:37:30 -0500

    fm with acc math behind it LOL (want to try and tweak sustain ASAP tho)

--- hardware/teensy/audio/src/piano.cpp
+++ hardware/teensy/audio/src/piano.cpp
@@ -16,16 +16,14 @@ AudioEffectEnvelope      env1;
 AudioEffectEnvelope      env2;
 AudioEffectEnvelope      env3;
 
-AudioMixer4              mixer; //CONTINUE HERE; TRY TO CHANNEL TWO AUDIOMIXER4 TO ADD MORE INHARMONIC HARMONICS; MUST CHANGE MEMORY TO 30 I THINK
+AudioMixer4              mixer; 
 AudioOutputI2S           i2sOutput;
 
 //AudioFilterStateVariable body;
 
-
-
 // Audio connections
 AudioConnection patch1(fmCarrier, 0, env, 0);   // Send carrier through envelope
-AudioConnection patch2(fmModulator, 0, fmCarrier, 0); // FM connection
+AudioConnection patch2(fmModulator, 0, fmCarrier, 0); // FM connection - THIS DOES THE MODULATION
 
 AudioConnection patch3(fmHarmonic1, 0, env1, 0); //send harmonic 1 to env1
 AudioConnection patch4(fmHarmonic2, 0, env2, 0);// send harmonic 2 to env2
@@ -38,9 +36,6 @@ AudioConnection patch9(env3, 0, mixer, 3); //send env to mixer audio input 3 (no
 
 AudioConnection patch10(mixer, 0, i2sOutput, 0); //take mixer and send it to i2s output, channel 0; left channel (of speaker/ headphones)
 AudioConnection patch11(mixer, 0, i2sOutput, 1); //take mixer and send it to i2s output, channel 1; right channel (of speaker/ headphones)
-//AudioConnection pBody(mixer, 1, body, 0);
-//AudioConnection pOutL(body, 0, i2sOutput, 0);
-//AudioConnection pOutR(body, 0, i2sOutput, 1);
 
 int note [] = {60, 62, 64, 65, 67, 69, 71, 72}; 
 int vel = 70; //fixed for now
@@ -56,10 +51,6 @@ void setup() {
   AudioMemory(20);
   audioShield.enable();
   audioShield.volume(0.4);
-  
-  //body.frequency(2000); 
-  //body.resonance(6);
-
 
   // Envelope setup
   env.attack(0.8); // fixed attack for now, can always integrate velocity here later using something like env.attack(3 - vel/70)
@@ -101,7 +92,6 @@ float velocityToAmp(byte velocity, unsigned int scaling_factor){
 void noteOn(byte channel, byte note, byte velocity) {
   noteIsOn = true;
 
-  //sustainLevel = 0.8; //resets to this every time a new note is played
   Serial.printf("Note ON: Ch %d, Note %d, Vel %d\n", channel, note, velocity);
   float freq = midiNoteToFreq(note);
   float amp = velocityToAmp(velocity, 5);
@@ -121,27 +111,11 @@ void noteOn(byte channel, byte note, byte velocity) {
   fmHarmonic3.amplitude(amp * 0.015);
  
 
-
 //turn on all envelopes
   env.noteOn(); //diff than this noteOn void function, acc envelope method
   env1.noteOn(); 
   env2.noteOn();
-  env3.noteOn();
-
-//keep on changing sustain &/ release part of ADSR until noteOff is called
-
-
-    /* initial logic, needs to change bc cannot depend on hold_time
- if (hold_time<400) sustainLevel=0.8; 
-    else if (hold_time<1000) sustainLevel=0.5;
-    else if (hold_time<2000) sustainLevel=0.3;
-    else sustainLevel=0.05; //assuming very long hold time
-    env.sustain(sustainLevel);
-    env1.sustain(sustainLevel);
-    env2.sustain(sustainLevel);
-    env3.sustain(sustainLevel);
-    */
-   
+  env3.noteOn();  
 }
 
 //when user releases the key, turn note OFF
@@ -154,7 +128,8 @@ void noteOff(byte channel, byte note, byte velocity){
   env3.noteOff();
 }
 
-
+//this is not doing anything rn bc you cant update sustain after starting the envelope
+/*
 void updateSustain(){
   if (noteIsOn){
     float decayFactor = 0.99; //has to be below 1.0, otherwise it is not decreasing the sustain
@@ -168,8 +143,7 @@ void updateSustain(){
     env3.sustain(sustainLevel);
   }
 }
-
-
+*/
 
 void loop() {
   for (int i=0; i<8; i++){
@@ -178,77 +152,4 @@ void loop() {
     noteOff(1, note[i], vel);
     delay(2000);
   }
-  
-  
-  //noteOff(1, 60, 60);
-
-  /*
-  for (int i = 0; i < 8; i++) {
-    
- 
-//change sustain pt of ADSR based on hold time
-    if (hold_time<400) sustainLevel=0.8;
-    else if (hold_time<1000) sustainLevel=0.5;
-    else if (hold_time<2000) sustainLevel=0.3;
-    else sustainLevel=0.05; //assuming very long hold time
-    env.sustain(sustainLevel);
-    env1.sustain(sustainLevel);
-    env2.sustain(sustainLevel);
-    env3.sustain(sustainLevel);
-    
-    //change decay pt of ADSR based on hold time
-    
-    decayTime = hold_time * 0.7;  // roughly fade for 70% of the hold time
-    if (decayTime > 4000) decayTime = 4000; // cap it so it doesn't last forever
-    if (decayTime < 200)  decayTime = 200;
-    env.decay(decayTime);
-    env1.decay(decayTime);
-    env2.decay(decayTime);
-    env3.decay(decayTime);
-/*
-    //change release pt of ADSR based on hold time
-    if (hold_time < 1000) releaseTime = 300;
-    else if (hold_time <2000) releaseTime = 600;
-    else if (hold_time>9000) releaseTime = 6000;
-    env.release(releaseTime);
-    env1.release(releaseTime);
-    env2.release(releaseTime);
-    env3.release(releaseTime);
-    env4.release(releaseTime);
-    env5.release(releaseTime);
-    */
-
-    /*
-  noteOn(1, note[i], vel);
-  delay(hold_time); //controls how long note stays in sustain stage (keeps note on until note off is called) -> ideally will not have this delay, the delay will be when the note off is called
-    noteOff(1, note[i], vel); //begins release stage
-    delay(releaseTime+200);
-
-    */
-
-  }
-
-
-//hold time logic needs to change bc we cannot take in hold time as a variable
-//will need to call note on when a note is played and the note will stop playing when the noteoff function is called
-//took out env4and5 from patches bc no longer needed
-//have on off switch for the note
-//mixer.vol (0.8) etc to set it instead of hold_time
-//everything should be in the note on and note off functions ideally
-
-
-
-
-
-
-    /*
-    //change decay pt of ADSR based on hold time
-    
-    decayTime = hold_time * 0.7;  // roughly fade for 70% of the hold time
-    if (decayTime > 4000) decayTime = 4000; // cap it so it doesn't last forever
-    if (decayTime < 200)  decayTime = 200;
-    env.decay(decayTime);
-    env1.decay(decayTime);
-    env2.decay(decayTime);
-    env3.decay(decayTime);
-    */
+}
